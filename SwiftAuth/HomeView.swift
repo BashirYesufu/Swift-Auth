8ef01254c4a91df449fed9c55c3f3e15269ae55d
bash <yesufubashir@gmail.com>
2023-06-14 00:50:48 +0100

    create home view

---
+++ SwiftAuth/HomeView.swift
@@ -0,0 +1,48 @@
+//
+//  HomeView.swift
+//  SwiftAuth
+//
+//  Created by Bash on 14/06/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var firstName: String = UserDefaults.standard.string(forKey: Keys.firstnameKey) ?? ""
+    @State private var username: String = UserDefaults.standard.string(forKey: Keys.usernameKey) ?? ""
+    @State private var email: String = UserDefaults.standard.string(forKey: Keys.emailKey) ?? ""
+    @State private var telephone: String = UserDefaults.standard.string(forKey: Keys.telephoneKey) ?? ""
+    
+    var body: some View {
+        ZStack {
+            Color.teal.ignoresSafeArea()
+            Circle()
+                .scale(1.7)
+                .foregroundColor(.white)
+                .opacity(0.15)
+            Circle()
+                .scale(1.4)
+                .foregroundColor(.white)
+            VStack {
+                Text(firstName)
+                    .font(.body)
+                    .foregroundColor(.teal)
+                Text(username)
+                    .font(.body)
+                    .foregroundColor(.teal)
+                Text(email)
+                    .font(.body)
+                    .foregroundColor(.teal)
+                Text(telephone)
+                    .font(.body)
+                    .foregroundColor(.teal)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
