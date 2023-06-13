5e398c7cc09191b3a54d458303946bf39b73e975
bash <yesufubashir@gmail.com>
2023-06-13 23:55:12 +0100

    Add registration view

--- SwiftAuth/HomeUIView.swift
+++ SwiftAuth/HomeUIView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct HomeUIView: View {
+    @State private var firstname = ""
+    @State private var username = ""
+    @State private var email = ""
+    @State private var telephone = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color.teal.ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white)
+                    .opacity(0.15)
+                Circle()
+                    .scale(1.4)
+                    .foregroundColor(.white)
+                
+                VStack {
+                    Text("Register")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("First name", text: $firstname)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    TextField("Email", text: $email)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    TextField("Telephone", text: $telephone)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    Button("Proceed") {
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(.teal)
+                    .cornerRadius(10)
+                }
+            }
+            .toolbar(.hidden, for: .navigationBar)
+            
+        }
     }
 }
 
